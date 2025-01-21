44ceb1ccc4ece5e2ff47f5a66c6244d9bb7482f1
Wenshin Lin <believeluckyseven@gmail.com>
2025-01-21 10:05:22 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 sudo apt update && \
 sudo apt install ffmpeg libsm6 libxext6  -y && \
 # pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113 && \
-!pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2 --extra-index-url https://download.pytorch.org/whl/cu118 && \
+pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2 --extra-index-url https://download.pytorch.org/whl/cu118 && \
 pip install "monai[nibabel, tqdm, einops]==1.2.0" && \
 pip install monailabel==0.7.0 && \
 pip install timm==0.6.13 && \
